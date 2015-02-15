1879ed5c3d8de0709fcc40af0c991aabfb0e238f
Raziq <mohideen.syed@rocketmail.com>
2015-02-15 05:12:24 -0500

    Changes

--- main.js
+++ main.js
@@ -7,20 +7,52 @@ var animate3 ;
 var imgObj4 = null;
 var animate4 ;
 
+var context = new AudioContext()
+var defaultTime = 500;
+
+var cOscillator = context.createOscillator(); cOscillator.frequency.value = 523.25;
+cOscillator.type = "square"
+var cGainNode = context.createGain()
+cOscillator.connect(cGainNode)
+cGainNode.connect(context.destination)
+cOscillator.start(0)
+cGainNode.gain.value = 0
+
+var dOscillator = context.createOscillator(); dOscillator.frequency.value = 587.33;
+dOscillator.type = "square"
+var dGainNode = context.createGain()
+dOscillator.connect(dGainNode)
+dGainNode.connect(context.destination)
+dOscillator.start(0)
+dGainNode.gain.value = 0
+
+var eOscillator = context.createOscillator(); eOscillator.frequency.value = 659.25;
+eOscillator.type = "square"
+var eGainNode = context.createGain()
+eOscillator.connect(eGainNode)
+eGainNode.connect(context.destination)
+eOscillator.start(0)
+eGainNode.gain.value = 0
+
+var fOscillator = context.createOscillator(); fOscillator.frequency.value = 698.46;
+fOscillator.type = "square"
+var fGainNode = context.createGain()
+fOscillator.connect(fGainNode)
+fGainNode.connect(context.destination)
+fOscillator.start(0)
+fGainNode.gain.value = 0
+
 function init(){
    imgObj = document.getElementById('myImage');
    imgObj.style.position= 'relative'; 
    imgObj.style.top = '70px'; 
    imgObj.style.left = '0px'; 
 }
-function init2()
-{
-
+function init2() {
    imgObj2 = document.getElementById('myImage2');
    imgObj2.style.position= 'relative'; 
    imgObj2.style.top = '220px'; 
    imgObj2.style.left = '0px'; 
-
 }
 function init3()
 {
@@ -94,53 +126,55 @@ function showImage4() {
     img.style.visibility = 'visible';
 }
 
-addEventListener("keydown", function(event) {
-    if (event.keyCode == 67)
-    {
-      init();
-      document.getElementById('c').play() 
-      showImage();
-      moveRight();
-      setInterval(function () {stop();}, 1500);
-    
-    }
-  });
+setInterval(function() {
+	var xmlHttp = new XMLHttpRequest()
+	xmlHttp.open("GET", "http://104.236.209.35:3000/sound", false)
+	xmlHttp.send(null)
+	var response = xmlHttp.responseText
+	switch(response) {
+		case "loud1":
+			gammaFunction(cGainNode, init, showImage, moveRight, stop)
+			break;
+		case "loud2":
+			gammaFunction(dGainNode, init2, showImage2, moveRight2, stop2)
+			break;
+		case "loud3":
+			gammaFunction(eGainNode, init3, showImage3, moveRight3, stop3)
+			break;
+		case "percussion":
+			gammaFunction(fGainNode, init4, showImage4, moveRight4, stop4)
+			break;
+		default:
+			break;
+	}
+}, defaultTime);
+
+function gammaFunction(gNode, ini, sho, mv, st) {
+	gNode.gain.value = 1
+	init()
+	sho()
+	mv()
+	setTimeout(function() {gNode.gain.value = 0; st();}, defaultTime)
+}
 
 addEventListener("keydown", function(event) {
-    if (event.keyCode == 68)
-    {
-       init2();
-       document.getElementById('d').play() 
-       showImage2();
-       moveRight2();
-      setInterval(function () {stop2();}, 1500);
-    }
-  });
-
-  
-  addEventListener("keydown", function(event) {
-    if (event.keyCode == 69)
-    {
-      init3();
-       document.getElementById('e').play() 
-       showImage3();
-        moveRight3();
-      setInterval(function () {stop3();}, 1500);
-    }
-  });
-
-
-  addEventListener("keydown", function(event) {
-    if (event.keyCode == 71)
-    {
-      init4();
-       document.getElementById('g').play() 
-       showImage4();
-       moveRight4();
-      setInterval(function () {stop4();}, 1500);
-    }
-  });
-  
+    switch(event.keyCode) {
+		case 67:
+			gammaFunction(cGainNode, init, showImage, moveRight, stop)
+			break;
+		case 68:
+			gammaFunction(dGainNode, init2, showImage2, moveRight2, stop2)
+			break;
+		case 69:
+			gammaFunction(eGainNode, init3, showImage3, moveRight3, stop3)
+			break;
+		case 70:
+			gammaFunction(fGainNode, init4, showImage4, moveRight4, stop4)
+			break;
+		default:
+			break;
+	}
+});
 
   
   
