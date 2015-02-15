f0eef855d1f02dae2c09410e2da743512e92a0a5
Raziq <mohideen.syed@rocketmail.com>
2015-02-15 07:58:35 -0500

    GG

--- main.js
+++ main.js
@@ -9,123 +9,63 @@ var animate4 ;
 
 var context = new AudioContext()
 var defaultTime = 500;
+var defaultFilter = 'bandpass';
+
+// var beatBuffer = null;
+// var request = new XMLHttpRequest();
+// request.open('GET', "http://104.236.209.35:3000/snare.wav", true);
+// request.responseType = 'arraybuffer';
+// request.onload = function() {
+// context.decodeAudioData(request.response, function(buffer) {
+	// beatBuffer = buffer;
+	// });
+// }
+// request.send();
 
 var cOscillator = context.createOscillator(); cOscillator.frequency.value = 523.25;
-cOscillator.type = "square"
 var cGainNode = context.createGain()
-cOscillator.connect(cGainNode)
+var cBQFilter = context.createBiquadFilter(); cBQFilter.type = defaultFilter
+cOscillator.connect(cBQFilter)
+cBQFilter.connect(cGainNode)
 cGainNode.connect(context.destination)
 cOscillator.start(0)
 cGainNode.gain.value = 0
 
 var dOscillator = context.createOscillator(); dOscillator.frequency.value = 587.33;
-dOscillator.type = "square"
 var dGainNode = context.createGain()
-dOscillator.connect(dGainNode)
+var dBQFilter = context.createBiquadFilter(); dBQFilter.type = defaultFilter
+dOscillator.connect(dBQFilter)
+dBQFilter.connect(dGainNode)
 dGainNode.connect(context.destination)
 dOscillator.start(0)
 dGainNode.gain.value = 0
 
 var eOscillator = context.createOscillator(); eOscillator.frequency.value = 659.25;
-eOscillator.type = "square"
 var eGainNode = context.createGain()
-eOscillator.connect(eGainNode)
+var eBQFilter = context.createBiquadFilter(); eBQFilter.type = defaultFilter
+eOscillator.connect(eBQFilter)
+eBQFilter.connect(eGainNode)
 eGainNode.connect(context.destination)
 eOscillator.start(0)
 eGainNode.gain.value = 0
 
 var fOscillator = context.createOscillator(); fOscillator.frequency.value = 698.46;
-fOscillator.type = "square"
 var fGainNode = context.createGain()
-fOscillator.connect(fGainNode)
+var fBQFilter = context.createBiquadFilter(); fBQFilter.type = defaultFilter
+fOscillator.connect(fBQFilter)
+fBQFilter.connect(fGainNode)
 fGainNode.connect(context.destination)
 fOscillator.start(0)
 fGainNode.gain.value = 0
 
-function init(){
-   imgObj = document.getElementById('myImage');
-   imgObj.style.position= 'relative'; 
-   imgObj.style.top = '70px'; 
-   imgObj.style.left = '0px'; 
-}
-function init2() {
-   imgObj2 = document.getElementById('myImage2');
-   imgObj2.style.position= 'relative'; 
-   imgObj2.style.top = '220px'; 
-   imgObj2.style.left = '0px'; 
-}
-function init3()
-{
-
-   imgObj3 = document.getElementById('myImage3');
-   imgObj3.style.position= 'relative'; 
-   imgObj3.style.top = '370px'; 
-   imgObj3.style.left = '0px'; 
-
-}
-function init4()
-{
-
-   imgObj4 = document.getElementById('myImage4');
-   imgObj4.style.position= 'relative'; 
-   imgObj4.style.top = '520px'; 
-   imgObj4.style.left = '0px'; 
-}
-
-function moveRight(){
-   imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
-   animate = setTimeout(moveRight,20); 
-}
-function stop(){
-   clearTimeout(animate);
-   imgObj.style.left = '0px'; 
-}
-
-function moveRight2(){
-   imgObj2.style.left = parseInt(imgObj2.style.left) + 10 + 'px';
-   animate2 = setTimeout(moveRight2,20); 
-}
-function stop2(){
-   clearTimeout(animate2);
-   imgObj2.style.top = '220px'; 
-   imgObj2.style.left = '0px'; 
-}
-function moveRight3(){
-   imgObj3.style.left = parseInt(imgObj3.style.left) + 10 + 'px';
-   animate3 = setTimeout(moveRight3,20); 
-}
-function stop3(){
-   clearTimeout(animate3);
-   imgObj3.style.top = '370px'; 
-   imgObj3.style.left = '0px'; 
-}
-function moveRight4(){
-   imgObj4.style.left = parseInt(imgObj4.style.left) + 10 + 'px';
-   animate4 = setTimeout(moveRight4,20); 
-}
-function stop4(){
-   clearTimeout(animate4);
-   imgObj4.style.top = '520px'; 
-   imgObj4.style.left = '0px'; 
-}
-
-function showImage() {
-    var img = document.getElementById('myImage');
-    img.style.visibility = 'visible';
-}
-function showImage2() {
-    var img = document.getElementById('myImage2');
-    img.style.visibility = 'visible';
-}
-function showImage3() {
-    var img = document.getElementById('myImage3');
-    img.style.visibility = 'visible';
-}
-function showImage4() {
-    var img = document.getElementById('myImage4');
-    img.style.visibility = 'visible';
-}
-
+var gOscillator = context.createOscillator(); gOscillator.frequency.value = 783.99;
+var gGainNode = context.createGain()
+var gBQFilter = context.createBiquadFilter(); gBQFilter.type = defaultFilter
+gOscillator.connect(gBQFilter)
+gBQFilter.connect(gGainNode)
+gGainNode.connect(context.destination)
+gOscillator.start(0)
+gGainNode.gain.value = 0
 
 setInterval(function() {
 	var xmlHttp = new XMLHttpRequest()
@@ -134,16 +74,22 @@ setInterval(function() {
 	var response = xmlHttp.responseText
 	switch(response) {
 		case "loud1":
-			gammaFunction(cGainNode, init, showImage, moveRight, stop)
+			gammaFunction(cGainNode, cOscillator)
 			break;
 		case "loud2":
-			gammaFunction(dGainNode, init2, showImage2, moveRight2, stop2)
+			gammaFunction(dGainNode, dOscillator)
 			break;
 		case "loud3":
-			gammaFunction(eGainNode, init3, showImage3, moveRight3, stop3)
+			gammaFunction(eGainNode, eOscillator)
 			break;
-		case "percussion":
-			gammaFunction(fGainNode, init4, showImage4, moveRight4, stop4)
+		case "loud4":
+			gammaFunction(fGainNode, fOscillator)
+			break;
+		case "loud5":
+			gammaFunction(gGainNode, gOscillator)
+			break;
+		case "percussion"
+			beatFunction()
 			break;
 		default:
 			break;
@@ -151,8 +97,13 @@ setInterval(function() {
 }, defaultTime);
 
 
-function gammaFunction(gNode, ini, sho, mv, st) {
-	//$('body').append($('<img src="note1.png" class="notes" />'));
+function gammaFunction(gNode, osc) {
+	var choices = ["sine", "square", "sawtooth", "triangle"]
+	var idx = Math.floor(Math.random() * choices.length);
+	osc.type = choices[idx]
+	
+	gNode.gain.value = 1
+	
 	createNewImage();
 	
 	var ww = $(window).width();
@@ -167,29 +118,37 @@ function gammaFunction(gNode, ini, sho, mv, st) {
 	$img = $(".notes");
 	wander();
 	
+	setTimeout(function() {gNode.gain.value = 0}, defaultTime)
+}
 
-  
-	gNode.gain.value = 1
-	init()
-	sho()
-	mv()
-	setTimeout(function() {gNode.gain.value = 0; st();}, defaultTime)
+function beatFunction() {
+	// var source = context.createBufferSource(); // creates a sound source
+	// source.buffer = beatBuffer;                    // tell the source which sound to play
+	// source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+	// source.start(0);  
+	var audio = new Audio('drum.wav');
+	audio.play();
 }
 
 addEventListener("keydown", function(event) {
     switch(event.keyCode) {
 		case 67:
-			gammaFunction(cGainNode, init, showImage, moveRight, stop)
+			gammaFunction(cGainNode, cOscillator)
 			break;
 		case 68:
-			gammaFunction(dGainNode, init2, showImage2, moveRight2, stop2)
+			gammaFunction(dGainNode, dOscillator)
 			break;
 		case 69:
-			gammaFunction(eGainNode, init3, showImage3, moveRight3, stop3)
+			gammaFunction(eGainNode, eOscillator)
+			break;
+		case 70:
+			gammaFunction(fGainNode, fOscillator)
 			break;
 		case 71:
-			gammaFunction(fGainNode, init4, showImage4, moveRight4, stop4)
+			gammaFunction(gGainNode, gOscillator)
 			break;
+		case 80:
+			beatFunction()
 		default:
 			break;
 	}
