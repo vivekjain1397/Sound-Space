5de556ddf7daf2a77cd57dff4469125792b61def
vivekjain1397 <vivekjain03@gmail.com>
2015-02-15 04:15:23 -0500

    notes only appear on keypress

--- main.js
+++ main.js
@@ -12,22 +12,34 @@ function init(){
    imgObj.style.position= 'relative'; 
    imgObj.style.top = '70px'; 
    imgObj.style.left = '0px'; 
+}
+function init2()
+{
 
    imgObj2 = document.getElementById('myImage2');
    imgObj2.style.position= 'relative'; 
    imgObj2.style.top = '220px'; 
    imgObj2.style.left = '0px'; 
 
+}
+function init3()
+{
+
    imgObj3 = document.getElementById('myImage3');
    imgObj3.style.position= 'relative'; 
    imgObj3.style.top = '370px'; 
    imgObj3.style.left = '0px'; 
+
+}
+function init4()
+{
 
    imgObj4 = document.getElementById('myImage4');
    imgObj4.style.position= 'relative'; 
    imgObj4.style.top = '520px'; 
    imgObj4.style.left = '0px'; 
 }
+
 function moveRight(){
    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
    animate = setTimeout(moveRight,20); 
@@ -64,12 +76,30 @@ function stop4(){
    imgObj4.style.top = '520px'; 
    imgObj4.style.left = '0px'; 
 }
-window.onload =init;
+
+function showImage() {
+    var img = document.getElementById('myImage');
+    img.style.visibility = 'visible';
+}
+function showImage2() {
+    var img = document.getElementById('myImage2');
+    img.style.visibility = 'visible';
+}
+function showImage3() {
+    var img = document.getElementById('myImage3');
+    img.style.visibility = 'visible';
+}
+function showImage4() {
+    var img = document.getElementById('myImage4');
+    img.style.visibility = 'visible';
+}
 
 addEventListener("keydown", function(event) {
     if (event.keyCode == 67)
     {
-      document.getElementById('c').play()  
+      init();
+      document.getElementById('c').play() 
+      showImage();
       moveRight();
       setInterval(function () {stop();}, 1500);
     
@@ -79,7 +109,9 @@ addEventListener("keydown", function(event) {
 addEventListener("keydown", function(event) {
     if (event.keyCode == 68)
     {
+       init2();
        document.getElementById('d').play() 
+       showImage2();
        moveRight2();
       setInterval(function () {stop2();}, 1500);
       
@@ -90,7 +122,9 @@ addEventListener("keydown", function(event) {
   addEventListener("keydown", function(event) {
     if (event.keyCode == 69)
     {
+      init3();
        document.getElementById('e').play() 
+       showImage3();
         moveRight3();
       setInterval(function () {stop3();}, 1500);
     }
@@ -100,7 +134,9 @@ addEventListener("keydown", function(event) {
   addEventListener("keydown", function(event) {
     if (event.keyCode == 71)
     {
+      init4();
        document.getElementById('g').play() 
+       showImage4();
        moveRight4();
       setInterval(function () {stop4();}, 1500);
     }
